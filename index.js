e2d158ba67f2f59c9a0a578f237dcc039cc3f404
fyx <2655251652@qq.com>
2024-06-14 19:22:23 +0800

    zancun

--- index.js
+++ index.js
@@ -13,22 +13,8 @@ const server = http.createServer((req, res) => {
     method: req.method,
     headers: req.headers,
   };
-//   console.log(options.path.search("https://"));
-  let proxyReq = undefined;
 
-  // 发送代理请求至目标服务器
-  //   if (true) {
-  //     proxyReq = https.request(options, (proxyRes) => {
-  //       res.writeHead(proxyRes.statusCode, proxyRes.headers);
-  //       proxyRes.pipe(res, {
-  //         end: true,
-  //       });
-  //     });
-  //     // 将客户端请求体发送至目标服务器
-  //     req.pipe(proxyReq, {
-  //       end: true,
-  //     });
-  //   } else {
+  let proxyReq = undefined;
   proxyReq = http.request(options, (proxyRes) => {
     res.writeHead(proxyRes.statusCode, proxyRes.headers);
     proxyRes.pipe(res, {
@@ -51,17 +37,45 @@ const server = http.createServer((req, res) => {
 
 server.on("connect", (req, clientSocket, head) => {
   const parts = req.url.split(":");
-  const upstream = net.createConnection(
-    parseInt(parts[1], 10),
-    parts[0],
-    () => {
-      clientSocket.write("HTTP/1.1 200 Connection Established\r\n\r\n");
-      upstream.write(head);
-      upstream.pipe(clientSocket);
-      clientSocket.pipe(upstream);
-    }
-  );
+  console.log(parts);
+  const option = {
+    hostname: parts[0],
+    port: parts[1], // 或者目标服务器的端口
+    path: req.url,
+    method: req.method,
+    headers: req.headers,
+  }
+  clientSocket.write("HTTP/1.1 200 Connection Established\r\n\r\n");
+  let p = http.request(option, (res) => {
+    // clientSocket.write(head)
+    res.pipe(clientSocket,{
+      end: true,
+    })
+  })
+  clientSocket.pipe(p,{
+    end: true,
+  });
+  p.on('error', (e) => {
+    console.log('socket 链接错误', e.message);
+  })
+
+  // const upstream = net.createConnection(
+  //   parseInt(parts[1], 10),
+  //   parts[0],
+  //   () => {
+  //     clientSocket.write("HTTP/1.1 200 Connection Established\r\n\r\n");
+  //     upstream.write(head);
+  //     upstream.pipe(clientSocket);
+  //     clientSocket.pipe(upstream);
+  //   }
+  // );
+  // upstream.on('error', (e) => {
+  //   console.log('socket 链接错误',e.message);
+  // })
 });
+server.on('error', (e) => {
+  console.log('http 服务出错', e);
+})
 
 // 设置代理服务器监听的端口
 const port = 443;
