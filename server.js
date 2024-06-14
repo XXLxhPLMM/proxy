73d4b798a2c315574cc5a78a58336a941f16eaf7
fyx <2655251652@qq.com>
2024-06-14 20:41:36 +0800

    服务端代码

--- server.js
+++ server.js
@@ -1,68 +1,140 @@
 const http = require("http");
 const net = require("net");
-// 代理服务器的地址和端口
 
-// 创建一个 HTTP 代理服务器
-const server = http.createServer((req, res) => {
 
-  // 构建代理请求
-  const options = {
-    hostname: req.headers.host,
-    port: new URL(req.url).port, // 或者目标服务器的端口
-    path: req.url,
-    method: req.method,
-    headers: req.headers,
-  };
+function run() {
+  // 创建一个 HTTP 代理服务器
+  console.log('启动服务中');
+  const server = http.createServer((req, res) => {
 
-  console.log(req.url, req.headers);
-  const proxyReq = http.request(options, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    proxyRes.pipe(res, {
+    // 构建代理请求
+    const options = {
+      hostname: req.headers.host,
+      port: new URL(req.url).port, // 或者目标服务器的端口
+      path: req.url,
+      method: req.method,
+      headers: req.headers,
+    };
+
+    console.log(req.url, req.headers);
+    const proxyReq = http.request(options, (proxyRes) => {
+      res.writeHead(proxyRes.statusCode, proxyRes.headers);
+      proxyRes.pipe(res, {
+        end: true,
+      });
+    });
+    // 将客户端请求体发送至目标服务器
+    req.pipe(proxyReq, {
       end: true,
     });
+
+
+    // 处理代理请求错误
+    proxyReq.on("error", (err) => {
+      console.error(err);
+      res.statusCode = 500;
+      res.end("Proxy request failed");
+    });
   });
-  // 将客户端请求体发送至目标服务器
-  req.pipe(proxyReq, {
-    end: true,
-  });
+
+  server.on("connect", (req, clientSocket, head) => {
+    try {
+      const parts = req.url.split(":");
+
+      // delete req.headers["proxy-authorization"]
+      req.headers["proxy-authorization"] = "true"
+      console.log(parts);
+      // ------------------------- 鉴权 --------------------------
+      if (req.headers["proxy-authorization"]) {
+        // console.log('关闭状态', clientSocket.closed);
+        const upstream = net.connect(
+          parseInt(parts[1], 10),
+          parts[0],
+          () => {
+            clientSocket.write("HTTP/1.1 200 Connection Established\r\n\r\n");
+            upstream.write(head);
+            console.log(new TextDecoder("utf-8").decode(head));
+          }
+        );
 
 
-  // 处理代理请求错误
-  proxyReq.on("error", (err) => {
-    console.error(err);
-    res.statusCode = 500;
-    res.end("Proxy request failed");
-  });
-});
+        // upstream.on('close', () => {
+        //   console.log('关闭');
+        // })
+        upstream.on('error', (e) => {
+          console.log('socket err:', e.message);
+          //  ---------------- 日志记录 -------------------
+        })
 
-server.on("connect", (req, clientSocket, head) => {
-  const parts = req.url.split(":");
+        // upstream.on('data',(data)=>{
+        //   //  ---------------- 拦截数据 -------------------
+        //   // console.log();
+        // })
 
-  console.log(req.headers);
-  const upstream = net.connect(
-    parseInt(parts[1], 10),
-    parts[0],
-    () => {
-      clientSocket.write("HTTP/1.1 200 Connection Established\r\n\r\n");
-      upstream.write(head);
+        // 链接双方管道
+        upstream.pipe(clientSocket);
+        clientSocket.pipe(upstream);
+        return
+      }
+      else {
+        clientSocket.destroy()
+        return
+      }
+
+    } catch {
 
     }
-  );
-  upstream.on('close',()=>{
-    console.log('关闭');
+  });
+
+  server.on('error', (e) => {
+    console.log('http 服务出错:', e);
+    //  ---------------- 日志记录 -------------------
   })
-  // 链接双方管道
-  upstream.pipe(clientSocket);
-  clientSocket.pipe(upstream);
+
+  // 设置代理服务器监听的端口
+  const port = 444;
+  server.listen(port, () => {
+    console.log(`Proxy Server running at http://localhost:${port}`);
+  });
+}
+
+function loopRun() {
+  return new Promise((res, rej) => {
+    try {
+      run()
+      // console.log('');
+    } catch {
+      res()
+    }
+  })
+}
+
+let count = -1
+let use = 0;
+const start = async () => {
+  while (true) {
+    use++
+    if (count > 0 && use < count) {
+      await loopRun()
+    } else if (count <= 0) {
+      await loopRun()
+    }
+    else {
+      break
+    }
+  }
+  console.log('服务结束');
+}
+start()
+process.on('uncaughtException', (error) => {
+  start()
+  console.error('Uncaught Exception:', error);
+  // 可以在这里进行日志记录或其他处理
+  // process.exit(1); // 退出进程，避免应用程序继续执行
 });
 
-server.on('request', (r, res) => {
-  // res.end()
-  console.log(r.method);
-})
-
-// 设置代理服务器监听的端口
-const port = 444;
-server.listen(port, () => {
-  console.log(`Proxy Server running at http://localhost:${port}`);
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  start()
+  // 可以在这里进行日志记录或其他处理
 });
