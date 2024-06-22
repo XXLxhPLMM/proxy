5dfa219584d11064d1e2f907ddd25f8449c2af94
fyx <2655251652@qq.com>
2024-06-22 17:13:21 +0800

    去除不必要依赖

---
+++ src/server/index.js
@@ -0,0 +1,93 @@
+import net from 'net'
+import { DecoderPipe, EncoderPipe } from '../utils/crypt';
+// function connectionListener
+
+
+
+function runServer(port) {
+    /**
+     * 创建服务
+     */
+    const server = net.createServer((client) => {
+
+        client.on('error', (e) => {
+            console.log('客户端错误');
+        })
+        // 解密管道
+        let dePipe = new DecoderPipe()
+        client.pipe(dePipe)
+
+        // 加密管道
+        let enPipe = new EncoderPipe()
+        dePipe.on('data', (data) => {
+            // 处理到目标链接的转发
+            // 解析客户端发来的 HTTP 请求头
+            const requestData = data.toString('utf-8');
+            const reqL = requestData.split('\r\n')[0].split(' ')
+            const method = reqL[0]
+            const target = reqL[1]
+            // 解析目标服务器的主机名和端口号
+            let serverHostname = ''
+            let serverPort = 80
+            if (method != 'connect' && method != 'CONNECT') {
+                const url = new URL(target)
+                serverHostname = url.hostname;
+                serverPort = url.port || 80
+                // console.log(url);
+            } else {
+                serverHostname = target.split(':')[0]
+                serverPort = target.split(':')[1] || 443
+            }
+            console.log(`请求: ${method}  ${target}:${port}`);
+            // return
+            // 创建一个与目标服务器的 TCP 连接
+            const serverSocket = net.connect({ host: serverHostname, port: serverPort }, () => {
+                console.log('已连接到目标服务器');
+                // 如果是 CONNECT 方法，向客户端发送确认
+                if (method === 'CONNECT' || method === 'connect') {
+                    // serverSocket.write(requestData)
+                    enPipe.write('HTTP/1.1 200 Connection Established\r\n\r\n')
+                    dePipe.pipe(serverSocket)
+                } else {
+                    // 向目标服务器发送客户端的请求数据
+                    serverSocket.write(data);
+                }
+            });
+
+            // 数据给加密管道
+            serverSocket.pipe(enPipe)
+            // 加密数据返回客户端
+            enPipe.pipe(client)
+            // 监听目标服务器断开连接事件
+             // -------------------------- 监听 关闭 和错误事件 及时释放连接 ---------------------
+            serverSocket.on('end', () => {
+                console.log('与目标服务器断开连接');
+                dePipe.end()
+                enPipe.end()
+                client.end();
+            });
+
+            // 监听目标服务器连接错误
+            serverSocket.on('error', (err) => {
+                console.error('目标服务器连接错误:', err);
+                client.end();
+            });
+
+            // -------------------------- 监听 关闭 和错误事件 及时释放连接 ---------------------
+            client.on('end', () => {
+                dePipe.end()
+                enPipe.end()
+                serverSocket.end()
+            })
+            client.on('error', () => {
+                dePipe.end()
+                enPipe.end()
+                serverSocket.end()
+            })
+        })
+    })
+
+    server.listen(port, () => {
+        console.warn('加密通道已启动 端口:', port);
+    })
+}
---
+++ src/utils/crypt.js
@@ -0,0 +1,62 @@
+import { Transform } from 'stream'
+import crypto from 'crypto'
+
+/**
+ * 密钥
+ */
+let key = "832c5fad98e071148c12fbb24ca99ba102b4cf0223d28f0388e95c7f208c969a"
+const encryptionKey = Buffer.from(key, 'hex'); // 32 bytes = 256 bits
+const iv = Buffer.from('f70f2b8b38630264f424a8a4c4c38c56', 'hex')
+// 加密函数
+function encryptText(text, key, iv) {
+    const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let encrypted = cipher.update(text);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return encrypted.toString('hex')
+}
+
+// 解密函数
+function decryptText(text, key, iv) {
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+    let decrypted = decipher.update(Buffer.from(text, 'hex'));
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+    return decrypted.toString();
+}
+
+
+/**
+ * 加密管道
+ */
+export class EncoderPipe extends Transform {
+    _transform(chunk, encoding, callback) {
+        // 加密
+        console.log('未加密数据', chunk.toString());
+        let data = encryptText(chunk, encryptionKey, iv)
+        console.log('加密数据', data);
+        callback(null, data); // 传递原始或修改后的数据
+    }
+
+    _flush(callback) {
+        // 调用 callback 以完成流处理
+        callback();
+    }
+}
+
+
+/**
+ * 解密管道
+ */
+export class DecoderPipe extends Transform {
+    _transform(chunk, encoding, callback) {
+        // 加密
+        console.log('未解密数据', chunk.toString());
+        let data = decryptText(chunk, encryptionKey, iv)
+        console.log('解密数据', data);
+        callback(null, data); // 传递原始或修改后的数据
+    }
+
+    _flush(callback) {
+        // 调用 callback 以完成流处理
+        callback();
+    }
+}
