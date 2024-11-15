fe9cd166fb2a219d052112d2952939bd2113f2a7
xxl <2655251652@qq.com>
2024-11-16 02:55:11 +0800

    打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 import path from "path";
-
+import CopyPlugin from "copy-webpack-plugin";
 export default {
   entry: "./src/manager/index.ts", // 你的入口文件
   target: "node", // 指定目标环境为 Node.js
@@ -13,20 +13,31 @@ export default {
       },
     ],
   },
-  stats:{
+  stats: {
     errorDetails: false, // 显示详细错误信息
   },
   resolve: {
     extensions: [".ts", ".js"], // 解析扩展名
   },
-  experiments:{
+  experiments: {
     outputModule: true, // 启用输出为 ES 模块
   },
   output: {
     filename: "app.js", // 输出文件名
     module: true, // 启用 ES 模块
+
     // format:"module", // 输出格式为 ES 模块
     path: path.resolve(process.cwd(), "dist"), // 输出目录
     clean: true, // 在每次构建之前清除文件
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "package.json",
+          to: "dist/package.json",
+        },
+      ],
+    }),
+  ],
 };
