12ea4efbb55d92de8151fb10f93d8e58398fbe73
xxl <2655251652@qq.com>
2024-11-24 23:20:58 +0800

    修复请求头丢失

--- src/server/httpProxy.ts
+++ src/server/httpProxy.ts
@@ -14,6 +14,7 @@ export function createHttpProxy(auth: (req: any, res: any) => Promise<boolean> =
         const proxyReq = http.request(`http://${host}`, {
             method: req.method,
             path: req.url,
+            headers: req.headers,
         }, (targetRes) => {
             targetRes.pipe(res);
         })
